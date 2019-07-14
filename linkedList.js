b9218fb2e3e2dd2428fd289e053307e99fc5e5c4
Keith <kayzee@live.com>
2019-07-13 19:55:54 -1000

    insert

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,73 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
+function linkedListGenerator() {
+  let head = null;
+  let tail = null;
 
+  function getHead() {
+    return head;
+  }
+  function getTail() {
+    return tail;
+  }
+
+  function add(value) {
+    if (head === null) {
+      head = {};
+      head.value = value;
+      head.next = null;
+      tail = head;
+      return head;
+    } else {
+      let newNode = {};
+      tail.next = newNode;
+      newNode.value = value;
+      newNode.next = null;
+      tail = newNode;
+      return newNode;
+    }
+  }
+
+  function get(index) {
+    let counter = 0;
+    let current = head;
+
+    while (current !== null) {
+      if (index === counter) {
+        return current;
+      } else {
+        current = current.next;
+        counter++;
+      }
+    }
+    return false;
+  }
+
+  function remove(index) {
+    let previousNode = get(index - 1);
+    let current = get(index);
+
+    if (index === 0) {
+      head = head.next;
+    } else if (current === tail) {
+      previousNode.next = null;
+      tail = previousNode;
+    } else if (current === false) {
+      return false;
+    } else {
+      previousNode.next = current.next;
+    }
+  }
+
+  function insert(value, index) {}
+
+  return {
+    getHead,
+    getTail,
+    add,
+    get,
+    remove,
+    insert
+  };
 }
